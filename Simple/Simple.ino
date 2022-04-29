b84db4d31324494956560eb75a21bdb66fd35cc0
geofftnz <geofftnz@gmail.com>
2022-04-29 23:58:53 +1200

    high speed pwm

--- Simple/Simple.ino
+++ Simple/Simple.ino
@@ -25,7 +25,10 @@
 #define TEMP_START (28<<2)
 #define FAN_SCALE 3
 #define FAN_MIN 32
+#define TEMP_BITS 10
 
+#define PWM_SHIFT 3
+#define PWM_MASK 0x1f
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
@@ -33,12 +36,29 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
 DeviceAddress addr;
+
 byte current = 0;
+int16_t millis_to_wait = 750;
+unsigned long temperature_read_time_ms = 0;
+byte pwm_counter = 0;
+unsigned int task_counter = 0;
+#define TASK_COUNTER_MASK 0x0fff
 
 void led(byte r, byte g, byte b){
   digitalWrite(LED_RED,1-r);
   digitalWrite(LED_GREEN,1-g);
   digitalWrite(LED_BLUE,1-b);
+}
+
+void requestTemperature()
+{
+  sensors.requestTemperaturesByAddress(addr);
+  temperature_read_time_ms = millis() + millis_to_wait;
+}
+
+bool readyToReadTemperature()
+{
+  return millis() > temperature_read_time_ms;
 }
 
 /*
@@ -54,8 +74,9 @@ void setup(void)
   sensors.begin();
 
   sensors.getAddress(addr,0);
-  sensors.setResolution(addr,10,true);
-  //sensors.setWaitForConversion(false);  // async mode, but we have to handle our own timing.
+  sensors.setResolution(addr,TEMP_BITS,true);
+  sensors.setWaitForConversion(false);  // async mode, but we have to handle our own timing.
+  millis_to_wait = sensors.millisToWaitForConversion(TEMP_BITS);
 
   pinMode(PWM_OUT,OUTPUT);
 
@@ -74,25 +95,18 @@ void setup(void)
   led(0,0,0);
 
   setFanSpeed(0);
-  
+  requestTemperature();
 }
 
 void setFanSpeed(byte newspeed)
 {
   // set LEDs
-  if (!newspeed) led(0,0,1);
+  if (!newspeed) led(0,0,0);
   else if (newspeed<=64) led(0,1,0);
   else if (newspeed<=128) led(1,1,0);
   else led(1,0,0);
-
   
-  // if we're starting the fan from a complete stop, drive it at full noise for a few cycles 
-  if (!current && newspeed){
-    analogWrite(PWM_OUT,255);
-    delay(1000);
-  }
-  current = newspeed;
-  analogWrite(PWM_OUT,current);
+  current = newspeed >> PWM_SHIFT;
 }
 
 
@@ -114,23 +128,47 @@ void processTemp(int16_t temp)
 /*
  * Main function, get and show the temperature
  */
+byte state = 0;
 void loop(void)
-{ 
-  sensors.requestTemperaturesByAddress(addr);
-
-  int16_t temp = sensors.getTemp(addr) >> 5;
-
-  // Check if reading was successful
-  if(temp != DEVICE_DISCONNECTED_RAW) 
+{   
+  // process pwm
+  byte pwm_counter_masked = pwm_counter & PWM_MASK;
+  
+  if (current && pwm_counter_masked == 0)
   {
-    processTemp(temp);
+      PORTB |= 0x01;
+  }
 
-    #ifdef SERIAL_ENABLE
-      Serial.print("temp,");
-      Serial.print(temp>>2);
-      Serial.print(",fan,");
-      Serial.println(current);
-    #endif
-   
-  } 
+  if (pwm_counter_masked == current)
+  {
+      PORTB &= ~0x01;
+  }
+
+  if (!pwm_counter)
+  {
+    task_counter++;
+    task_counter &= TASK_COUNTER_MASK;
+    if (task_counter == 0)
+    {
+      if (readyToReadTemperature())
+      {
+          int16_t temp = sensors.getTemp(addr) >> 5;
+          
+          if(temp != DEVICE_DISCONNECTED_RAW) 
+          {
+              processTemp(temp);
+              
+              #ifdef SERIAL_ENABLE
+              Serial.print("temp,");
+              Serial.print(temp>>2);
+              Serial.print(",fan,");
+              Serial.println(current);
+              #endif
+          } 
+          requestTemperature();
+      }
+    }
+  }
+    
+  pwm_counter++;
 }
