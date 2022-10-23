7d75afa7ee3748dbf5279f8f93767fbd70f663c6
geofftnz <geofftnz@gmail.com>
2022-10-24 10:03:05 +1300

    fix LED outputs.

--- Simple/Simple.ino
+++ Simple/Simple.ino
@@ -21,16 +21,16 @@
 #define LED_BLUE 1
 
 // temp to turn off fan
-#define TEMP_OFF (25<<2)
+#define TEMP_OFF (27<<2)
 
 // temp to start the fan
-#define TEMP_START (28<<2)
+#define TEMP_START (29<<2)
 
-// bitshift amount to scale fan speed with temp
-#define FAN_SCALE 2
+// bitshift amount to scale fan speed with temp (where temp is <<2)
+#define FAN_SCALE 1
 
-// minimum PWM value
-#define FAN_MIN 64
+// minimum PWM value (note that this will get shifted right by PWM_SHIFT)
+#define FAN_MIN 24
 
 // resolution of temp sensor
 #define TEMP_BITS 10
@@ -38,9 +38,24 @@
 #define PWM_SHIFT 3
 #define PWM_MASK 0x1f
 
-// these will need to be defined based on output pin. These are writing to port B output 0, which is pin 5 on the ATTiny85
-#define FAN_ON PORTB |= 0x01
-#define FAN_OFF PORTB &= ~0x01
+#define SETPORT(x) PORTB = (PORTB & 0xF0) | ((x) & 0x0F)
+
+byte portbstate[4]={
+  0x0E, // Idle: blue led off
+  0x0C, // Idle: blue led on
+  0x0A, // Running: fan off, green led on    
+  0x07  // Running: fan on, red led on  
+};
+
+#define IDLE0 portbstate[0]
+#define IDLE1 portbstate[1]
+#define FAN0 portbstate[2]
+#define FAN1 portbstate[3]
+
+// states, used as indexes into pairs of values in the portbstate array
+#define STATE_IDLE 0
+#define STATE_RUN 2
+byte state = STATE_IDLE;
 
 // how often to check temperature. When the task counter masked with this hits zero, the temperature will be checked. 0x0fff is about 1 sec period.
 #define TASK_COUNTER_MASK 0x3fff
@@ -53,7 +68,8 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DeviceAddress addr;
 
-byte current = 0;
+byte pwm_level = 0; // point at which pwm switched from on to off, within the range set by PWM_MASK.
+
 int16_t millis_to_wait = 750;
 unsigned long temperature_read_time_ms = 0;
 byte pwm_counter = 0;
@@ -94,13 +110,11 @@ void setup(void)
   millis_to_wait = sensors.millisToWaitForConversion(TEMP_BITS);
 
   pinMode(PWM_OUT,OUTPUT);
-
   pinMode(LED_RED,OUTPUT);
   pinMode(LED_GREEN,OUTPUT);
   pinMode(LED_BLUE,OUTPUT);
 
-  setFanSpeed(255);
-
+  SETPORT(FAN1);  // fan on
   led(1,0,0);  delay(100);
   led(1,1,0);  delay(100);
   led(0,1,0);  delay(100);
@@ -108,67 +122,75 @@ void setup(void)
   led(0,0,1);  delay(100);
   led(1,0,1);  delay(100);
   led(0,0,0);
-
-  setFanSpeed(0);
-  requestTemperature();
-}
-
-void setFanSpeed(byte newspeed)
-{
-  // set LEDs
-  if (!newspeed) led(0,0,0);
-  else if (newspeed<=64) led(0,1,0);
-  else if (newspeed<=128) led(1,1,0);
-  else led(1,0,0);
-
-  newspeed >>= PWM_SHIFT;
-  // give a stopped fan a kick-start
-  if (newspeed && !current){
-      FAN_ON;
-      delay(500);
-      FAN_OFF;
-  }
+  SETPORT(FAN0); // fan off
   
-  current = newspeed;
+  requestTemperature();
 }
 
 
 void processTemp(int16_t temp)
 {
-  if (temp < TEMP_OFF){
-    setFanSpeed(0);    
+  byte newState = state;
+
+  switch(state)
+  {
+    case STATE_IDLE:
+      // if we've exceeded our turn-on temp, start the fan.
+      if (temp >= TEMP_START){
+
+        // kickstart the fan
+        SETPORT(FAN1);  //red + fan
+        delay(1000);
+        SETPORT(0x08);  // blue + green while we wait for initial temp adjust
+        
+        // set the pwm to minimum
+        pwm_level = FAN_MIN >> PWM_SHIFT;
+        newState = STATE_RUN;
+      }
+    break;
+    
+    case STATE_RUN:
+
+      // if we've dropped below our turn-off temp, revert to idle state
+      if (temp <= TEMP_OFF){
+        newState = STATE_IDLE;
+        pwm_level = 1;
+      }
+      else{
+        // calculate fan speed 
+        int16_t s = (((temp - TEMP_OFF) << FAN_SCALE) + FAN_MIN);
+        if (s<0) s = 0;
+        if (s>255) s = 255;
+        pwm_level = (s >> PWM_SHIFT) & PWM_MASK;
+      }
+    
+    break;
   }
-  if (temp >= TEMP_START){
-    int16_t s = (temp - TEMP_START) << FAN_SCALE;
-    if (s<0) s = 0;
-    if (s>(255-FAN_MIN)) s = (255-FAN_MIN);
-    if (s) s+= FAN_MIN;
-    setFanSpeed((byte)s);
-  }
+
+  state = newState;
 }
 
 
 /*
  * Main function, get and show the temperature
  */
-byte state = 0;
 void loop(void)
 {   
   // process pwm
   byte pwm_counter_masked = pwm_counter & PWM_MASK;
 
-  // if our pwm counter has reached our fan setting, then turn off
-  if (pwm_counter_masked == current)
+  // if our pwm counter has reached our pwm level, then turn off...
+  if (pwm_counter_masked == pwm_level)
   {
-      FAN_OFF;
+      SETPORT(portbstate[state]);
+  }
+  // otherwise if we've wrapped around to zero, turn on. Note that if pwm_level == 0, we wont reach this point.
+  else if (pwm_counter_masked == 0)
+  {
+      SETPORT(portbstate[state+1]);
   }
 
-  // if our current fan speed value is not zero, and we're at the start of our pwm cycle, then turn on
-  if (current && pwm_counter_masked == 0)
-  {
-      FAN_ON;
-  }
-
+  // increment task counter when pwm_counter hits zero
   if (!pwm_counter)
   {
     task_counter++;
@@ -187,7 +209,7 @@ void loop(void)
               Serial.print("temp,");
               Serial.print(temp>>2);
               Serial.print(",fan,");
-              Serial.println(current);
+              Serial.println(pwm_level);
               #endif
           } 
           requestTemperature();
